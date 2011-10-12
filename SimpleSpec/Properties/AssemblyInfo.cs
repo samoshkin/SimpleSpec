e23384a2ba37fc32ea25cbaf88ae3a2389e5fe7e
Alexey Samoshkin <al.samoshkin@gmail.com>
2011-10-12 11:38:56 +0300

    Added general and version information about the assembly.

--- SimpleSpec/Properties/AssemblyInfo.cs
+++ SimpleSpec/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleSpec")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SimpleSpec - is a simple BDD framework for .NET of xSpec type. Specifications are written at a unit level.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleSpec")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Alexey Samoshkin 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1")]
